8f00bccdfda01058cdddddff01f02fd221289ca9
Cécile Tonglet <cecile.tonglet@gmail.com>
2014-10-02 13:09:08 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name = "restoredb",
+    version = "1.0",
+    author = "Cecile Tonglet",
+    author_email = "cecile.tonglet@gmail.com",
+    description = ("Database restoration script to automatically decompress "
+                   "any format and pipe to the right restorer"),
+    license = "MIT",
+    keywords = "postgres database dump restore",
+    url = "https://github.com/cecton/restoredb.py",
+    py_modules = ['restoredb'],
+    scripts = ['restoredb.py'],
+    install_requires = ['StreamDecompressor'],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Database",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
