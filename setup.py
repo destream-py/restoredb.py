c6f5425fe844a9eb09cf3b6cc53acc3b82c899b7
Cécile Tonglet <cecile.tonglet@gmail.com>
2014-12-22 10:24:33 +0100

    release 1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "restoredb",
-    version = "1.0",
+    version = "1.1",
     author = "Cecile Tonglet",
     author_email = "cecile.tonglet@gmail.com",
     description = ("Database restoration script to automatically decompress "
